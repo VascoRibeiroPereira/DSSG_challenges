7ab2f3e8255e67afd0e7f27acbb31b9d71386f69
VascoRibeiroPereira <vascoribeiro.sintra@gmail.com>
2022-04-24 23:37:35 +0100

    Freedom code initial commit. WordClouds created from road names and tweets

---
+++ freedom/script/liberdade.R
@@ -0,0 +1,126 @@
+# Libs
+library(dplyr)
+library(sf)
+library(ggplot2)
+library(rtweet)
+library(readr)
+library(tm)
+library(RWeka)
+library(wordcloud2) 
+library(webshot)
+library(htmlwidgets)
+
+# Twitter
+
+## Not Run
+# appname <- "xxx"
+# key = "xxx"
+# secret = "xxx"
+# access_token = "xxx-xxx"
+# access_secret = "xxx"
+# 
+# twitter_token <- create_token(
+#   app = appname,
+#   consumer_key = key,
+#   consumer_secret = secret,
+#   access_token = access_token,
+#   access_secret = access_secret)
+# 
+# freedom_tweets <- search_tweets(q = "#25deAbril",
+#                                n = 500)
+# 
+# write_csv(freedom_tweets, "freedom/files/tweets.csv")
+
+freedom_tweets = read_csv("freedom/files/tweets.csv")
+
+corpus <- Corpus(VectorSource(freedom_tweets$text))
+
+corpus <- freedom_tweets$text %>% 
+  removePunctuation() %>% 
+  removeWords(stopwords(kind = "pt")) %>% 
+  removeWords(stopwords(kind = "es")) %>%
+  stripWhitespace() %>% 
+  VectorSource() %>% 
+  Corpus()
+
+tdm <- TermDocumentMatrix(corpus)
+
+m <- as.matrix(tdm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+
+# DSSGPT
+# freedom_link = "https://github.com/dssgPT/Plotting-Good-DSSG/blob/main/desafios/003_Liberdade_OSM/ruas_abril.geojson?raw=true"
+# download.file(freedom_link, "freedom/files/freedom_data.geojson")
+# freedom_data <- geojson_read("freedom/files/freedom_data.geojson",  what = "sp") # missing some data
+
+# All PT road names
+#download.file(pt_url,"freedom/files/pt_raw") ## do not run more than once
+#unzip("freedom/files/pt_raw.zip") ## do not run more than once
+pt_roads_raw <- st_read("freedom/files/pt_raw/gis_osm_roads_free_1.shp")
+pt_roads <- pt_roads_raw$name
+pt_roads <- pt_roads[!is.na(pt_roads)] %>% tolower() # remove NA and remove uppers as a variable
+
+sorted_roads = table(pt_roads) %>% as.data.frame() %>% arrange(desc(Freq)) %>% mutate(ppm = Freq/sum(Freq)*1000000) # sort from more frequent to least frequent with ‰
+
+sorted_roads %>% head()
+
+# PT expressions associated with 25 April
+
+freedom_expr = c("25 de abril", 
+                 "liberdade", 
+                 "salgueiro maia", 
+                 "capitães de abril", 
+                 "movimento das forças armadas", 
+                 "mfa", 
+                 "spínola",
+                 "spinola",
+                 "cravos",
+                 "revolução")
+
+
+freedom_roads = tibble()
+
+for (i in freedom_expr) {
+  
+  tmp = grep(i, sorted_roads$pt_roads)
+  
+  freedom_roads = bind_rows(sorted_roads[tmp,], freedom_roads)
+}
+
+## arrange data and remove duplicates
+freedom_roads = freedom_roads %>% arrange(desc(Freq))
+duplicate_roads = freedom_roads %>% duplicated()
+freedom_roads = freedom_roads[!duplicate_roads,]
+
+
+# WordCloud for street names
+
+set.seed(1234) # for reproducibility 
+wordcloud2(data=freedom_roads[,1:2], size=3, color='random-light', backgroundColor="black")
+saveWidget(test,"tmp.html",selfcontained = F)
+
+# Wordcloud for Tweets
+
+set.seed(1234) # for reproducibility 
+wordcloud2(data=d, size=3, color='random-light', backgroundColor="black")
+
+# How important is freedom for Portugal?
+
+highlight_df = inner_join(freedom_roads,sorted_roads[1:20,])
+
+sorted_roads[1:20,] %>% 
+  ggplot(aes(reorder(pt_roads, ppm), ppm)) + 
+  geom_col(aes(fill = ppm)) + 
+  scale_fill_gradient2(low = "darkblue", 
+                       high = "darkgreen", 
+                       midpoint = median(sorted_roads[1:20,]$ppm)) +
+  geom_col(data=highlight_df, aes(pt_roads,ppm),color="red",fill=NA) +
+  # geom_point(data=highlight_df, 
+  #            aes(pt_roads,ppm), 
+  #            color='red',
+  #            size=3) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8)) +
+  labs(x = "Rua") +
+  coord_flip()
