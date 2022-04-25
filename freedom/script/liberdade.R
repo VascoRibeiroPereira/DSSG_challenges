8dbb19c76ab14ebfe6455ef333a691cbfe4ecdb7
VascoRibeiroPereira <vascoribeiro.sintra@gmail.com>
2022-04-25 17:03:45 +0100

    Ficheiros .csv gráficos obtidos de posts twitter e nomes de ruas

--- freedom/script/liberdade.R
+++ freedom/script/liberdade.R
@@ -27,7 +27,9 @@ library(htmlwidgets)
 #   access_secret = access_secret)
 # 
 # freedom_tweets <- search_tweets(q = "#25deAbril",
-#                                n = 500)
+#                                n = 2500)
+# 
+# freedom_tweets = freedom_tweets %>% filter(grepl("Portugal", location))
 # 
 # write_csv(freedom_tweets, "freedom/files/tweets.csv")
 
@@ -39,6 +41,7 @@ corpus <- freedom_tweets$text %>%
   removePunctuation() %>% 
   removeWords(stopwords(kind = "pt")) %>% 
   removeWords(stopwords(kind = "es")) %>%
+  removeWords(stopwords(kind = "en")) %>%
   stripWhitespace() %>% 
   VectorSource() %>% 
   Corpus()
@@ -48,7 +51,9 @@ tdm <- TermDocumentMatrix(corpus)
 m <- as.matrix(tdm)
 v <- sort(rowSums(m),decreasing=TRUE)
 d <- data.frame(word = names(v),freq=v)
-head(d, 10)
+d = d[-grep("https", d$word),] ## remove links as terms
+d = d %>% filter(!(word %in% c("leões", "fcporto", "dia"))) ## remove footbal terms
+#head(d, 10)
 
 # DSSGPT
 # freedom_link = "https://github.com/dssgPT/Plotting-Good-DSSG/blob/main/desafios/003_Liberdade_OSM/ruas_abril.geojson?raw=true"
@@ -94,33 +99,34 @@ freedom_roads = freedom_roads %>% arrange(desc(Freq))
 duplicate_roads = freedom_roads %>% duplicated()
 freedom_roads = freedom_roads[!duplicate_roads,]
 
+write_csv(freedom_tweets, "freedom/files/freedom_roads.csv")
 
-# WordCloud for street names
 
-set.seed(1234) # for reproducibility 
-wordcloud2(data=freedom_roads[,1:2], size=3, color='random-light', backgroundColor="black")
-saveWidget(test,"tmp.html",selfcontained = F)
+# Printing Images
 
-# Wordcloud for Tweets
+## Wordcloud for Tweets
 
 set.seed(1234) # for reproducibility 
-wordcloud2(data=d, size=3, color='random-light', backgroundColor="black")
+tweet_cloud = wordcloud2(data=d, size=3, color='random-light', backgroundColor="black")
+saveWidget(tweet_cloud,"freedom/files/tweets.html",selfcontained = F)
+webshot::webshot("freedom/files/tweets.html","freedom/files/tweets.png",vwidth = 680, vheight = 680, delay =10)
 
-# How important is freedom for Portugal?
+
+## How important is freedom in Portugal?
 
 highlight_df = inner_join(freedom_roads,sorted_roads[1:20,])
 
-sorted_roads[1:20,] %>% 
+roads_img = sorted_roads[1:20,] %>% 
   ggplot(aes(reorder(pt_roads, ppm), ppm)) + 
   geom_col(aes(fill = ppm)) + 
   scale_fill_gradient2(low = "darkblue", 
                        high = "darkgreen", 
                        midpoint = median(sorted_roads[1:20,]$ppm)) +
   geom_col(data=highlight_df, aes(pt_roads,ppm),color="red",fill=NA) +
-  # geom_point(data=highlight_df, 
-  #            aes(pt_roads,ppm), 
-  #            color='red',
-  #            size=3) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8)) +
   labs(x = "Rua") +
   coord_flip()
+
+png(filename = "freedom/files/freedom_roads.png", width = 680, height = 680, res = 150)
+roads_img
+dev.off()
